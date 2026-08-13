85611661dff3cce0791c0460daff051f08d0717f
Claire <137269854+ecleaire@users.noreply.github.com>
2026-08-14 03:18:39 +0900

    Connect countdown configuration modules

---
+++ docs/wro-countdown/v2/config.js
@@ -0,0 +1,5 @@
+// Public configuration API used by the countdown modules.
+export * from "./config-values.js";
+export * from "./config-normalize.js";
+export * from "./config-time.js";
+export * from "./config-storage.js";
