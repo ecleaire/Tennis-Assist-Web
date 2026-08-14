bf13da53ed3917304d4fd6628cca314175efedfe
Claire <137269854+ecleaire@users.noreply.github.com>
2026-08-14 12:13:36 +0900

    Register placement settings controls

--- docs/wro-countdown/v2/controls.js
+++ docs/wro-countdown/v2/controls.js
@@ -1,4 +1,4 @@
-import { $ } from "./ui.js";
+import { $ } from "./ui.js?v=20260814j";
 
 const IDS = [
   "modeTimer", "modeWro", "targetTimeField", "targetTime",
@@ -8,16 +8,27 @@ const IDS = [
   "autoWroDuration", "themeDark", "themeLight", "autoSize",
   "clockSizeRange", "clockSize", "timerSizeRange", "timerSize",
   "targetSizeRange", "targetSize", "subSizeRange", "subSize",
-  "timerTextSizeRange", "timerTextSize", "noiseRange",
-  "noiseStrength", "noisePattern", "noiseInterval", "lineGap",
-  "noisePreview", "alarmEnabled", "atTarget", "customLead",
-  "addLead", "leadChips", "soundType", "volumeRange", "volume",
-  "audioFile", "fileName", "testSound", "removeAudio",
-  "audioStatus"
+  "timerTextSizeRange", "timerTextSize",
+  "clockOffsetX", "clockOffsetY", "timerOffsetX", "timerOffsetY",
+  "wroOffsetX", "wroOffsetY",
+  "noiseRange", "noiseStrength", "noisePattern", "noiseInterval",
+  "lineGap", "noisePreview", "alarmEnabled", "atTarget",
+  "customLead", "addLead", "leadChips", "soundType",
+  "volumeRange", "volume", "audioFile", "fileName", "testSound",
+  "removeAudio", "audioStatus"
 ];
 
 export function controls() {
   const result = Object.fromEntries(IDS.map(id => [id, $(id)]));
   result.leadPresets = [...document.querySelectorAll(".leadPreset")];
+  result.clockPositionChoices = [
+    ...document.querySelectorAll('input[name="clockPosition"]')
+  ];
+  result.timerPositionChoices = [
+    ...document.querySelectorAll('input[name="timerPosition"]')
+  ];
+  result.wroPositionChoices = [
+    ...document.querySelectorAll('input[name="wroPosition"]')
+  ];
   return result;
 }
