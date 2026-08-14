27a393357720599cfea3b450e9710b7aebef1156
Claire <137269854+ecleaire@users.noreply.github.com>
2026-08-14 14:27:44 +0900

    Refresh countdown configuration modules

--- docs/wro-countdown/v2/config.js
+++ docs/wro-countdown/v2/config.js
@@ -1,5 +1,5 @@
 // Public configuration API used by the countdown modules.
-export * from "./config-values.js?v=20260814j";
-export * from "./config-normalize.js?v=20260814j";
-export * from "./config-time.js";
-export * from "./config-storage.js?v=20260814j";
+export * from "./config-values.js?v=20260814m";
+export * from "./config-normalize.js?v=20260814m";
+export * from "./config-time.js?v=20260814m";
+export * from "./config-storage.js?v=20260814m";
