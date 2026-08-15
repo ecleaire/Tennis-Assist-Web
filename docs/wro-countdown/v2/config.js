fa83a26efd5db566484490888692f3e238158e78
Claire <137269854+ecleaire@users.noreply.github.com>
2026-08-15 23:48:00 +0900

    Refresh expanded sound configuration

--- docs/wro-countdown/v2/config.js
+++ docs/wro-countdown/v2/config.js
@@ -1,5 +1,5 @@
 // Public configuration API used by the countdown modules.
-export * from "./config-values.js?v=20260815f";
-export * from "./config-normalize.js?v=20260815f";
+export * from "./config-values.js?v=20260815h";
+export * from "./config-normalize.js?v=20260815h";
 export * from "./config-time.js?v=20260815d";
-export * from "./config-storage.js?v=20260815f";
+export * from "./config-storage.js?v=20260815h";
