40522b41624c66912fe784b32ac226f73d6cb45f
Claire <137269854+ecleaire@users.noreply.github.com>
2026-08-19 02:17:31 +0900

    Load the new default layout and alert presets

--- docs/wro-countdown/v2/config-normalize.js
+++ docs/wro-countdown/v2/config-normalize.js
@@ -3,7 +3,7 @@ import {
   PATTERNS,
   POSITION_VALUES,
   SOUND_TYPES
-} from "./config-values.js?v=20260815h";
+} from "./config-values.js?v=20260815i";
 import { SIZE_LIMITS } from "./size-limits.js?v=20260815f";
 
 export const clamp = (value, minimum, maximum) =>
