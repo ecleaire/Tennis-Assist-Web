66bef8fb26d25de4941e2b7527e6ba3f29e22ec2
Claire <137269854+ecleaire@users.noreply.github.com>
2026-08-21 01:43:04 +0900

    Refresh hardened settings normalization

--- docs/wro-countdown/v2/config.js
+++ docs/wro-countdown/v2/config.js
@@ -1,5 +1,5 @@
 // Public configuration API used by the countdown modules.
 export * from "./config-values.js?v=20260820b";
-export * from "./config-normalize.js?v=20260820b";
+export * from "./config-normalize.js?v=20260821a";
 export * from "./config-time.js?v=20260815d";
-export * from "./config-storage.js?v=20260820b";
+export * from "./config-storage.js?v=20260821a";
