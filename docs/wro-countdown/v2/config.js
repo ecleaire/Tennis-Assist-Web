d6b92a6254c00aaac25d41dd22827a711e643c46
Claire <137269854+ecleaire@users.noreply.github.com>
2026-08-21 02:31:44 +0900

    Export completion message sequence configuration

--- docs/wro-countdown/v2/config.js
+++ docs/wro-countdown/v2/config.js
@@ -1,5 +1,6 @@
 // Public configuration API used by the countdown modules.
-export * from "./config-values.js?v=20260820b";
-export * from "./config-normalize.js?v=20260821a";
+export * from "./config-values.js?v=20260821b";
+export * from "./config-normalize.js?v=20260821b";
 export * from "./config-time.js?v=20260815d";
-export * from "./config-storage.js?v=20260821a";
+export * from "./config-storage.js?v=20260821b";
+export * from "./completion-messages.js?v=20260821b";
