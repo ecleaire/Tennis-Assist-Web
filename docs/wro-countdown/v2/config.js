13a53b00b0ca2c1853e7055b6790dad1601016f8
Claire <137269854+ecleaire@users.noreply.github.com>
2026-08-21 11:08:42 +0900

    Export the expanded timer-size normalizer

--- docs/wro-countdown/v2/config.js
+++ docs/wro-countdown/v2/config.js
@@ -1,6 +1,6 @@
 // Public configuration API used by the countdown modules.
 export * from "./config-values.js?v=20260821c";
-export * from "./config-normalize.js?v=20260821c";
+export * from "./config-normalize-timer.js?v=20260821d";
 export * from "./config-time.js?v=20260815d";
-export * from "./config-storage.js?v=20260821c";
+export * from "./config-storage.js?v=20260821d";
 export * from "./completion-messages.js?v=20260821c";
