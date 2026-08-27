baa74c74388ebe5734de01a88ea603161230426f
Claire <137269854+ecleaire@users.noreply.github.com>
2026-08-27 14:49:49 +0900

    Normalize all composite settings controls before layout

--- docs/wro-countdown/v2/main.js
+++ docs/wro-countdown/v2/main.js
@@ -20,6 +20,9 @@ import {
   installTextAutoSizeSettings,
   createTextAutoSizeController
 } from "./text-auto-size-settings.js?v=20260821f";
+import {
+  installSettingsFormIntegrity
+} from "./settings-form-integrity.js?v=20260821g";
 import { installSettingsLayout } from "./settings-layout.js?v=20260821b";
 import { controls as makeControls } from "./controls.js?v=20260820b";
 import { renderSettings } from "./render-settings.js?v=20260820b";
@@ -43,6 +46,7 @@ installTextAutoSizeSettings();
 installDefaultSettingsUi();
 installBackgroundSettings();
 installCompletionAutoWroSetting();
+installSettingsFormIntegrity();
 installSettingsLayout();
 applySizeLimits();
 
